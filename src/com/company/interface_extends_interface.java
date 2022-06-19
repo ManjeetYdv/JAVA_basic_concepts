448b8f81bfbbb6c192a658652ab423c1f23d2114
ManjeetYdv <ymanjeet2003@gmail.com>
2022-06-19 14:40:26 +0530

    interface_extends_interface

---
+++ src/com/company/interface_extends_interface.java
@@ -0,0 +1,22 @@
+package com.company;
+
+interface  i1{
+    void run();
+}
+interface xyz extends i1{
+    void walk();
+}
+class classxyz implements xyz{
+    public void walk(){
+
+    }
+    public void run(){
+
+    }
+}
+
+public class interface_extends_interface{
+    public static void main(String[] args) {
+
+    }
+}
